533f07cdd1ca68dc9a1f6aec3ed20286097d228b
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-26 21:05:57 +0530

    added student dashboard to root

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/public/Landing";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import StudentDashboard from "./pages/student/Dashboard";
 
 const AppRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/student/dashboard" element={<StudentDashboard />} />
     </Routes>
   );
 };
