817e11180473077144a4f9da79b85304e960ea81
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-27 18:10:47 +0530

    Added My issues to route

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -11,6 +11,9 @@ import Register from "./pages/auth/Register";
 import StudentDashboard from "./pages/student/Dashboard";
 import ReportIssue from "./pages/student/ReportIssue";
 
+import MyIssues from "./pages/student/MyIssues";
+
+
 const AppRoutes = () => {
   return (
     <Routes>
@@ -24,6 +27,7 @@ const AppRoutes = () => {
       {/* Student */}
       <Route path="/student/dashboard" element={<StudentDashboard />} />
       <Route path="/student/report" element={<ReportIssue />} />
+      <Route path="/student/issues" element={<MyIssues />} />
     </Routes>
   );
 };
