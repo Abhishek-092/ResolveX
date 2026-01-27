a810efd46a4585ded7021c91292b206fb3011eec
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-27 17:36:10 +0530

    updated Routes

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,17 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 
+// Public
 import Landing from "./pages/public/Landing";
+
+// Auth
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+
+// Student
 import StudentDashboard from "./pages/student/Dashboard";
+import ReportIssue from "./pages/student/ReportIssue";
 
 const AppRoutes = () => {
   return (
     <Routes>
+      {/* Public */}
       <Route path="/" element={<Landing />} />
+
+      {/* Auth */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+      {/* Student */}
       <Route path="/student/dashboard" element={<StudentDashboard />} />
+      <Route path="/student/report" element={<ReportIssue />} />
     </Routes>
   );
 };
