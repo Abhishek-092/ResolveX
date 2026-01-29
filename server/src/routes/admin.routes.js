b6c0e90cf1a4e68a2d36f86d133e459ac744b01a
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-30 05:05:23 +0530

    admin route added

---
+++ server/src/routes/admin.routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { protect } = require("../middlewares/auth.middleware");
+const { requireAdmin } = require("../middlewares/role.middleware");
+
+const {
+  getAllIssues,
+  assignIssue,
+  updateIssueStatus,
+  mergeIssues
+} = require("../controllers/admin.controller");
+
+const router = express.Router();
+
+// admin-only
+router.use(protect, requireAdmin);
+
+router.get("/issues", getAllIssues);
+router.patch("/issues/:id/assign", assignIssue);
+router.patch("/issues/:id/status", updateIssueStatus);
+router.patch("/issues/:id/merge", mergeIssues);
+
+module.exports = router;
