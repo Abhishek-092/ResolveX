5f4bac5d7506fc23656debef42795544bc39f88e
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-30 04:50:50 +0530

    name update

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
   register,
   login
-} = require("../controllers/auth.controllers");
+} = require("../controllers/auth.controller");
 
 const router = express.Router();
 
