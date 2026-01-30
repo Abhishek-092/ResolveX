09f33152e528ad09ff1e1e9ea9251fb8b1131cd4
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-30 21:30:43 +0530

    updated

--- server/src/middleware/role.middleware.js
+++ server/src/middleware/role.middleware.js
@@ -1,4 +1,4 @@
-exports.requireAdmin = (req, res, next) => {
+export const requireAdmin = (req, res, next) => {
   if (req.user.role !== "admin") {
     return res.status(403).json({ message: "Admin access only" });
   }
