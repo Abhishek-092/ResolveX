9b09a352b24e60c94178ac908e7ba4d02a40d65f
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-31 02:17:09 +0530

    updated

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,10 +1,9 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
-module.exports = {
-  PORT: process.env.PORT || 5000,
-  MONGO_URI: process.env.MONGO_URI,
-  JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRES_IN: "7d",
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
-};
+export const PORT = process.env.PORT || 5000;
+export const MONGO_URI = process.env.MONGO_URI;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_EXPIRES_IN = "7d";
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
