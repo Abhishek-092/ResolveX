d721f72564a1ffd595a8bc5361be7df2004aec31
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-04-27 09:25:08 +0530

    feat: initialize react application entry point with auth and router providers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,19 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
-import { AuthProvider } from "./context/AuthContext";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import { AuthProvider } from './context/AuthContext';
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const container = document.getElementById('root');
+const root = createRoot(container);
 
 root.render(
-  <AuthProvider>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </AuthProvider>
+  <React.StrictMode>
+    <AuthProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </AuthProvider>
+  </React.StrictMode>
 );
